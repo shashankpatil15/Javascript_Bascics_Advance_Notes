9dc95d0e095e3580b00fb0d882b189bd0eaa9a56
sweetyWankhade <sweety.wankhade@lonartech.com>
2025-09-17 21:35:51 +0530

    Functions

---
+++ Functions.js
@@ -0,0 +1,32 @@
+
+//  Functions
+// function functionName(){
+//     // code to be executed
+// }
+// console.log(functionName());
+
+// var result = 1+2;
+// console.log(result);
+
+// function addTwoNumers(num1,num2){
+//     return num1 + num2;
+// }
+// console.log(addTwoNumers(10,30));
+
+// var muliply = function(num1,num2){
+//     return num1 * num2;
+// }
+
+// console.log(muliply(10,20));
+
+// var divide = (num1,num2)=>{
+//     return num1 / num2;
+// }
+// console.log(divide(20,10));
+
+// selft invoking function
+
+(function(){
+    console.log("selft invoking function");
+})();
+
